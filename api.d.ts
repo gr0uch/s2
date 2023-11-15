ad6ebd3d0c0e69fb34da6b0d6ee208562e15265f
lyric <lyricwulf@gmail.com>
2023-11-15 14:44:33 -0800

    add root fn props to ts def

--- api.d.ts
+++ api.d.ts
@@ -1,12 +1,12 @@
 type PrimitiveKey = string | number | symbol;
 
 interface DataObject {
-    [key: PrimitiveKey]: any,
+  [key: PrimitiveKey]: any;
 }
 
 // Mainly for annotation purposes.
 interface Proxy {
-    [key: PrimitiveKey]: any,
+  [key: PrimitiveKey]: any;
 }
 
 type S2Result = [
@@ -26,9 +26,38 @@ export function html(
   ...props: Array<string | Element>
 ): Element;
 
-export function observable(obj?: DataObject, shouldPartiallyReplace?: boolean): Proxy;
+export function observable(
+  obj?: DataObject,
+  shouldPartiallyReplace?: boolean
+): Proxy;
 export function computed(definition: DataObject): DataObject;
 export function ref(obj: DataObject): DataObject;
+
+type S2Fn = ((obj: DataObject, template: Element) => S2Result) & {
+  /**
+   * enabled by default, but can be disabled. This will automatically call
+   * unmount when the DOM nodes mapped to an object are removed. This should
+   * only be disabled if you need to keep updating nodes that may be removed and
+   * appended later.
+   */
+  shouldUnmountRoot: boolean;
+  /**
+   * (experimental): this will defer setting proxy values as a microtask. This
+   * might be preferable if there is significant blocking in between updates.
+   * However, it can break functionality in case there are updates that depend
+   * on a previous update in the same tick.
+   */
+  isDeferred: boolean;
+  /**
+   * set a different global object for server-side rendering
+   */
+  window: Window | null;
+  /**
+   * use comment nodes and turn on messages in the console. Warning: has a
+   * performance impact.
+   */
+  debug: boolean;
+};
 
 export default function s2(obj: DataObject, template: Element): S2Result;
 
